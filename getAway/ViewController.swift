024cec1b487c032f9621fa0c006681084e5b18af
seocho <whtjdqls617@gmail.com>
2022-08-11 16:05:15 +0900

    Initial Commit

---
+++ getAway/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  getAway
+//
+//  Created by 조성빈 on 2022/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
